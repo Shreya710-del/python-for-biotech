fd10aa65b50946ca09679e7bcc9558aa469c01b4
Shreya Singh <shreya7102008@gmail.com>
2025-12-24 22:06:07 +0530

    Add initial DNA sequence printing script
    Initial implementation of a DNA sequence script that prints the sequence and its length.

---
+++ dna_basics.py
@@ -0,0 +1,15 @@
+dna = "ATGCGTAACGTT"
+
+print("DNA sequence:", dna)
+print("Length:", len(dna))
+dna = "ATGCGTAACGTT"
+
+print("DNA sequence:", dna)
+print("Length:", len(dna))dna = "ATGCGTAACGTT"
+
+print("DNA sequence:", dna)
+print("Length:", len(dna))dna = "ATGCGTAACGTT"
+
+print("DNA sequence:", dna)
+print("Length:", len(dna))
+Add first DNA script
